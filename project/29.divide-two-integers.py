efde6889834baf71af147a0aa8aa75262e66cdbc
adonis <adonis.lau@123>
2020-04-28 21:13:08 +0800

    29. Divide Two Integers

--- project/29.divide-two-integers.py
+++ project/29.divide-two-integers.py
@@ -4,28 +4,30 @@
 """
 29. Divide Two Integers
 https://leetcode.com/problems/divide-two-integers/
+解题思路：
+因为不能使用乘除法，且暴力加减会超时，所以需要使用位运算
+每次循环中，设置了一个temp，让dividend每次除以双倍的（prev）temp，又抵消掉了很多多余的遍历
 """
 
 
 class Solution:
-    def divide(self, a: int, b: int) -> int:
-        # 确定运算结果的正负号
-        is_negative = -1 if (a < 0 and b >= 0) or (a >= 0 and b < 0) else 1
-        res = is_negative * self.helper(abs(a), abs(b))
-        return min(res, 2 ** 31 - 1)
-
-    def helper(self, a, b):
-        if b == 1: return a
+    def divide(self, dividend: int, divisor: int) -> int:
+        # 这里设的也很巧妙， 同号为TRUE，异号False
+        positive = (dividend < 0) is (divisor < 0)
+        dividend, divisor = abs(dividend), abs(divisor)
         res = 0
-        while a >= b:
-            t, i = b, 1
-            while a >= t:
-                a -= t
+        while dividend >= divisor:
+            temp, i = divisor, 1
+            while dividend >= temp:
+                dividend -= temp
                 res += i
+                # 这里的temp 左移*2， 对应i*2 也是为了做temp的记录，可以减少时间复杂度
                 i <<= 1
-                t <<= 1
-        return res
+                temp <<= 1
+        if not positive:
+            res = -res
+        return min(max(-2147483648, res), 2147483647)
 
 
 solution = Solution()
-print(solution.divide(10, 3))
+print(solution.divide(20, 3))
