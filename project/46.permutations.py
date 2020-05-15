3de2479860f063efc46b9ddce3ccb32649d420c5
adonis <adonis.lau@123>
2020-05-15 18:06:18 +0800

    46. Permutations

--- project/46.permutations.py
+++ project/46.permutations.py
@@ -12,22 +12,25 @@ class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         # 直接使用库函数
         # return list(itertools.permutations(nums))
-        def backtrack(first=0):
-            # 所有数都填完了
-            if first == n:
-                res.append(nums[:])
-            for i in range(first, n):
-                # 动态维护数组，变换待填数字的位置
-                nums[first], nums[i] = nums[i], nums[first]
-                # 继续递归填下一个数
-                backtrack(first + 1)
-                # 撤销操作
-                nums[first], nums[i] = nums[i], nums[first]
 
-        n = len(nums)
-        res = []
-        backtrack()
-        return res
+        ans = []
+
+        def dfs(nums, path):
+            # 当前路径满足条件
+            if path and len(path) == len(nums):
+                ans.append(list(path))
+
+            # 在剩余的数字中选择下一个数字
+            for num in (set(nums) - set(path)):
+                path.append(num)
+                dfs(nums, path)
+                # 回溯
+                path.pop()
+            return ans
+
+        path = list()
+        dfs(nums, path)
+        return ans
 
 
 solution = Solution()
