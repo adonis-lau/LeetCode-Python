d734cbf4f25668274430e798b317bea443b03a3c
adonis <adonis.lau.dev@gmail.com>
2020-07-07 22:45:32 +0800

    77. Combinations

--- project/77.combinations.py
+++ project/77.combinations.py
@@ -13,6 +13,9 @@ from typing import List
 
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
+        # 使用 Itertools 工具类
+        # return itertools.combinations([i + 1 for i in range(n)], k)
+
         # 将 nums 初始化为从 1 到 k的整数序列。nums就是每次向output中添加的结果
         # 将 n + 1添加为末尾元素，起到“哨兵”的作用。
         nums = list(range(1, k + 1)) + [n + 1]
