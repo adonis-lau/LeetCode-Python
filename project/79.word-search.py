991626e474367be1593d55abf3d3af560140037d
adonis <adonis.lau.dev@gmail.com>
2020-07-08 23:37:42 +0800

    79. Word Search

---
+++ project/79.word-search.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+
+"""
+79. Word Search
+https://leetcode.com/problems/word-search/
+解题思路：
+递归，回溯
+上右下左的顺序遍历二维数组当前值的周围值：
+如果匹配成功，继续按照上右下左的顺序遍历。匹配不到就回溯
+"""
+from typing import List
+
+
+class Solution:
+    #         (x-1,y)
+    # (x,y-1) (x,y) (x,y+1)
+    #         (x+1,y)
+
+    directions = [(0, -1), (-1, 0), (0, 1), (1, 0)]
+
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        m = len(board)
+        if m == 0:
+            return False
+        n = len(board[0])
+
+        marked = [[False for _ in range(n)] for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                # 遍历二维数组（因为不知道哪个字母与word首字母相同，所以只能遍历二维数组）
+                if self.__search_word(board, word, 0, i, j, marked, m, n):
+                    return True
+        return False
+
+    def __search_word(self, board, word, index,
+                      start_x, start_y, marked, m, n):
+        # 先写递归终止条件
+        if index == len(word) - 1:
+            return board[start_x][start_y] == word[index]
+
+        # 中间匹配了，再继续搜索
+        if board[start_x][start_y] == word[index]:
+            # 先占住这个位置，搜索不成功的话，要释放掉
+            marked[start_x][start_y] = True
+            # 进行上下左右搜索
+            for direction in self.directions:
+                new_x = start_x + direction[0]
+                new_y = start_y + direction[1]
+                # 下标没有越界 && 当前值没有被遍历过 && 二维数组当前值能匹配到word当前值
+                # 注意：如果这一次 search word 成功的话，就返回
+                if 0 <= new_x < m and 0 <= new_y < n and \
+                        not marked[new_x][new_y] and \
+                        self.__search_word(board, word,
+                                           index + 1,
+                                           new_x, new_y,
+                                           marked, m, n):
+                    return True
+            marked[start_x][start_y] = False
+        return False
+
+
